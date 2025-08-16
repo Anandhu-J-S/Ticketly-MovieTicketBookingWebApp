a124745f949859210db6208307b02d5057f044d3
ANANDHUJS-Srishti <anandhujs.srishti@gmail.com>
2025-08-16 14:44:44 +0530

    commit from ubuntu

--- Server/src/Models/userModel.js
+++ Server/src/Models/userModel.js
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-    profilePic:{type:String, required:true},
+    profilePic: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     contact: { type: Number, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, required: true, enum: ["admin", "user", "theater"] }
+    role: { type: String, required: true, enum: ["admin", "user", "theater"] },
+    isActive:{
+        type:String,
+        required:true,
+        default:"pending"
+    }
 }, {
     timestamps: true
 })
